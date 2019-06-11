6e460cb715a816796771dffd0c856dc3c4d79e54
Jiyu <jiyu.shao@gmail.com>
2019-06-11 16:47:01 +0800

    update dilithium wabpack version

--- dilithium/webpack.config.js
+++ dilithium/webpack.config.js
@@ -1,30 +1,23 @@
-let webpack = require('webpack');
-let CircularDependencyPlugin = require('circular-dependency-plugin')
+let CircularDependencyPlugin = require("circular-dependency-plugin");
 
 module.exports = {
-  entry: './dilithium.js',
+  mode: "development",
+  entry: "./dilithium.js",
   output: {
     path: __dirname,
-    filename: 'build/Dilithium.js',
-    libraryTarget: 'var',
-    library: 'Dilithium'
+    filename: "build/Dilithium.js",
+    libraryTarget: "var",
+    library: "Dilithium"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
-        loader: 'babel',
-        query: {
-          plugins: [
-            ['transform-react-jsx', {pragma: 'Dilithium.createElement'}],
-            'transform-class-properties'
-          ]
+        use: {
+          loader: "babel-loader"
         }
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new CircularDependencyPlugin(),
-  ]
-}
+  plugins: [new CircularDependencyPlugin()]
+};
