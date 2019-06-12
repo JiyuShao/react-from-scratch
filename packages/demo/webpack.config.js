7207c7d848e9ecfab0d45243fc09e46856c799fa
Jiyu <jiyu.shao@gmail.com>
2019-06-12 11:37:53 +0800

    change demo webpack dev server setting

--- packages/demo/webpack.config.js
+++ packages/demo/webpack.config.js
@@ -1,18 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  mode: "development",
-  entry: "./app.js",
-  output: {
-    path: __dirname,
-    filename: "bundle.js"
+  mode: 'development',
+  entry: './app.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Dilithium demo',
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+  ],
 };
